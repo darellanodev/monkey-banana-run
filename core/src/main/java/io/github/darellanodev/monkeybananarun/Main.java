07668eacd861e5844bbc705eb36bc53d5a1c816c
darellanodev <darellano.aliaga@gmail.com>
2025-12-10 20:17:24 +0100

    feat: Create a viewport in Main class

--- core/src/main/java/io/github/darellanodev/monkeybananarun/Main.java
+++ core/src/main/java/io/github/darellanodev/monkeybananarun/Main.java
@@ -6,10 +6,12 @@ import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
+    private FitViewport viewport;
     private Texture image;
     private Texture background;
     private Music music;
@@ -25,6 +27,8 @@ public class Main extends ApplicationAdapter {
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        viewport.apply();
+        batch.setProjectionMatrix(viewport.getCamera().combined);
         batch.begin();
         batch.draw(image, 140, 210);
         batch.end();
@@ -34,5 +38,10 @@ public class Main extends ApplicationAdapter {
     public void dispose() {
         batch.dispose();
         image.dispose();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
     }
 }
